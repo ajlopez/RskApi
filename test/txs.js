325b9cfd70893c13ad65739dfeb728461df0b84b
ajlopez <webmaster@ajlopez.com>
2017-06-25 15:35:25 -0300

    Get transaction by hash

---
+++ test/txs.js
@@ -0,0 +1,32 @@
+
+var rskapi = require('..');
+
+exports['get transaction by hash'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getTransactionByHash = function (hash) {
+		return {
+			hash: hash
+		};
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getTransactionByHash('0x1234', function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(data.hash, '0x1234');
+		test.done();
+	});
+};
+
+function createProvider() {
+	return {
+		call: function (method, args, cb) {
+			cb(null, this[method].apply(this,args));
+		}
+	}
+}
+
