5e0395f622c70b4ea239c99265377c3f94cd2b51
ajlopez <webmaster@ajlopez.com>
2017-06-25 15:47:46 -0300

    Get transaction receipt by hash

--- test/txs.js
+++ test/txs.js
@@ -22,6 +22,27 @@ exports['get transaction by hash'] = function (test) {
 	});
 };
 
+exports['get transaction receipt by hash'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getTransactionReceipt = function (hash) {
+		return {
+			hash: hash
+		};
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getTransactionReceiptByHash('0x1234', function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(data.hash, '0x1234');
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
