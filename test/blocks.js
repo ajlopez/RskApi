77651cb36475c9d1b784e67e3931da4a9c0240fa
ajlopez <webmaster@ajlopez.com>
2017-06-25 14:05:10 -0300

    Get block by hash

--- test/blocks.js
+++ test/blocks.js
@@ -43,6 +43,30 @@ exports['get block by number'] = function (test) {
 	});
 };
 
+exports['get block by hash'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getBlock = function (hash) {
+		return {
+			hash: hash
+		}
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getBlockByHash('0x1234', function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(typeof data, 'object');
+		
+		test.equal(data.hash, '0x1234');
+		
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
