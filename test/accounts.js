7fd5fa433fca97de69220c8755368e69e0bb35a5
ajlopez <webmaster@ajlopez.com>
2017-06-26 14:39:22 -0300

    Get balance

---
+++ test/accounts.js
@@ -0,0 +1,30 @@
+
+var rskapi = require('..');
+
+exports['get balance'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getBalance = function (hash) {
+		return '0x2a'
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getBalance('0x1234', function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(data, '0x2a');
+		test.done();
+	});
+};
+
+function createProvider() {
+	return {
+		call: function (method, args, cb) {
+			cb(null, this[method].apply(this,args));
+		}
+	}
+}
+
