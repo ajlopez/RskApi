69dbc3b2b6b88463190f38269da055ca9cd261c7
ajlopez <webmaster@ajlopez.com>
2017-06-27 13:03:02 -0300

    Get account transaction count by hash

--- test/accounts.js
+++ test/accounts.js
@@ -20,6 +20,26 @@ exports['get balance'] = function (test) {
 	});
 };
 
+exports['get transaction count'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getTransactionCount = function (hash, block) {
+		test.equal(block, 'latest');
+		return '0x2a'
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getTransactionCount('0x1234', function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(data, '0x2a');
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
