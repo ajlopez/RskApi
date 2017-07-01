8abce4b16fa83b7a11607262fa395cd95d5801b6
ajlopez <webmaster@ajlopez.com>
2017-07-01 14:21:40 -0300

    Get accounts sample

---
+++ samples/simple/getaccounts.js
@@ -0,0 +1,13 @@
+
+var rskapi = require('../..');
+
+var host = rskapi.host(process.argv[2]);
+
+host.getAccounts(function (err, data) {
+	if (err)
+		console.log('error', err);
+	else
+		console.dir(data);
+});
+
+
