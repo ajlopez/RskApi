6b1772bf0050821d58575d69f27b472e499d74f3
ajlopez <webmaster@ajlopez.com>
2017-09-16 16:33:15 -0300

    Send transaction

--- test/txs.js
+++ test/txs.js
@@ -43,6 +43,33 @@ exports['get transaction receipt by hash'] = function (test) {
 	});
 };
 
+exports['send transaction'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_sendTransaction = function (data) {
+		return data;
+	};
+	
+	var host = rskapi.host(provider);
+	
+	var txdata = {
+		from: '0x01',
+		to: '0x02',
+		gasPrice: 1,
+		gas: 21000,
+		value: 10000
+	}
+	
+	host.sendTransaction(txdata, function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.deepEqual(data, txdata);
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
