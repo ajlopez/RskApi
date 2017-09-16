a671015b38467c064e3aa7354a5be5acb8233f19
ajlopez <webmaster@ajlopez.com>
2017-09-16 16:44:50 -0300

    Send transfer sample

---
+++ samples/simple/sendtx.js
@@ -0,0 +1,26 @@
+
+var rskapi = require('../..');
+
+var host = rskapi.host(process.argv[2]);
+
+var from = process.argv[3];
+var to = process.argv[4];
+var value = process.argv[5];
+
+var txdata = {
+	from: from,
+	to: to,
+	value: value,
+	gas: 21000,
+	gasPrice: 1
+}
+
+
+host.sendTransaction(txdata, function (err, data) {
+	if (err)
+		console.log('error', err);
+	else
+		console.dir(data);
+});
+
+
