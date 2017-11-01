a8f8e08955baf59c3605ad01ffb39110df54fd79
ajlopez <webmaster@ajlopez.com>
2017-11-01 11:00:37 -0300

    Fix new personal account sample

--- samples/simple/newpaccount.js
+++ samples/simple/newpaccount.js
@@ -2,7 +2,7 @@
 var rskapi = require('../..');
 
 var host = rskapi.host(process.argv[2]);
-var passphrase = process.argv[2];
+var passphrase = process.argv[3];
 
 host.newPersonalAccount(passphrase, function (err, data) {
 	if (err)
