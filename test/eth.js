9e7d9a13c5f1265665bb0695dc6d1584bc3329a8
ajlopez <webmaster@ajlopez.com>
2017-11-18 11:35:28 -0300

    Get syncing

--- test/eth.js
+++ test/eth.js
@@ -58,6 +58,43 @@ exports['get compilers'] = function (test) {
 	});
 };
 
+exports['get compilers'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_compilers = function () {
+		return [ "solidity" ];
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getCompilers(function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.deepEqual(data, [ "solidity" ]);
+		test.done();
+	});
+};
+
+exports['get syncing info'] = function (test) {
+	var provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_syncing = function () {
+		return false;
+	};
+	
+	var host = rskapi.host(provider);
+	
+	host.getSyncing(function (err, data) {
+		test.equal(err, null);
+		test.strictEqual(data, false);
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
