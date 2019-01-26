3822ce9f17a02bd07b9970570b31905f99fe47b6
ajlopez <webmaster@ajlopez.com>
2019-01-26 16:31:50 -0300

    Async await http test

---
+++ test/asynchttp.js
@@ -0,0 +1,73 @@
+
+var rskapi = require('..');
+
+var methods = {
+	eth_blockNumber: function () {
+		return 42;
+	}
+};
+
+var server;
+
+exports['start http server'] = function (test) {
+	test.async();
+	
+	server = createServer(methods);
+	
+	server.listen(3000, function (err) {
+		if (err)
+			console.log(err);
+		else
+			test.done();
+	});
+};
+	
+exports['get block number'] = async function (test) {
+	const host = rskapi.host('http://localhost:3000');
+	
+	const number = await host.getBlockNumber();
+    
+    test.equal(number, 42);
+    test.done();
+};
+
+exports['stop http server'] = function (test) {
+	test.async();
+	
+	server.close(function (err) {
+		if (err)
+			console.log(err);
+		else
+			test.done();
+	});
+};
+
+function createServer(methods) {
+	return require('http').createServer(function (req, res) {		
+		if (req.method === 'POST') {
+			var body = '';
+
+			req.on('data', function (data) {
+				body += data;
+			});
+
+			req.on('end', function () {
+				var input = JSON.parse(body);
+				
+				var data = {
+					id: input.id,
+					jsonrpc: input.data
+				};
+				
+				if (!methods[input.method])
+					data.error = { code: -32061, message: 'Method not found' };
+				else
+					data.result = methods[input.method].apply(methods, input.params);
+				
+				res.write(JSON.stringify(data));
+				
+				res.end();
+			});
+		}
+	});	
+}
