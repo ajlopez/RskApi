9aefb612a2c4583b57ab2e63521b3ab44f0948f3
ajlopez <webmaster@ajlopez.com>
2019-09-01 16:24:40 -0300

    Samples call command

---
+++ samples/commands/call.js
@@ -0,0 +1,45 @@
+
+const rskapi = require('../..');
+const simpleabi = require('simpleabi');
+const utils = require('./lib/utils');
+const txs = require('./lib/txs');
+
+let config;
+
+try {
+    config = require('./config.json');
+}
+catch (ex) {
+    config = {};
+}
+
+if (!config.instances)
+    config.instances = {};
+
+if (!config.options)
+    config.options = {};
+
+const from = utils.getAddress(config, process.argv[2]);
+const to = utils.getInstanceAddress(config, process.argv[3]);
+const fn = process.argv[4];
+let args = process.argv[5];
+
+if (args)
+    args = args.split(',');
+
+const tx = {
+    from: from,
+    to: to,
+    value: 0,
+    gas: config.options.gas || 5000000,
+    gasPrice: config.options.gasPrice || 60000000,
+    data: simpleabi.encodeCall(fn, args)
+};
+
+const host = rskapi.host(config.host);
+
+(async function() {
+    const result = await txs.call(host, tx);
+    console.log('result', result);
+})();
+
--- samples/commands/lib/utils.js
+++ samples/commands/lib/utils.js
@@ -15,6 +15,19 @@ function getAddress(config, user) {
     return user;
 }
 
+function getInstanceAddress(config, name) {
+    if (name.substring(0, 2).toLowerCase() === '0x')
+        return name;
+    
+    if (!config.instances)
+        return name;
+    
+    if (config.instances[name])
+        return config.instances[name].address || config.instances[name];
+    
+    return name;
+}
+
 function getAccount(config, user) {
     if (user.substring(0, 2).toLowerCase() === '0x')
         return user;
@@ -37,6 +50,7 @@ function getValue(value) {
 
 module.exports = {
     getAddress: getAddress,
+    getInstanceAddress: getInstanceAddress,
     getAccount: getAccount,
     getValue: getValue
 };
