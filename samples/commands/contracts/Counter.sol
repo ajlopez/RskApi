c5cdb1308314f686fee37d341e0a51ea13af76c9
ajlopez <webmaster@ajlopez.com>
2019-09-01 15:39:20 -0300

    Counter contract

---
+++ samples/commands/contracts/Counter.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Counter {
+    uint public counter;
+    
+    function increment() public {
+        counter++;
+    }
+    
+    function add(uint value) public {
+        counter += value;
+    }
+}
+
+
