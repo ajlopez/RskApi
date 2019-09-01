5d1efafd32a47e2fdb128c4a801d1e15c30a3379
ajlopez <webmaster@ajlopez.com>
2019-09-01 16:31:12 -0300

    Samples invoke command

---
+++ samples/commands/invoke.js
@@ -0,0 +1,46 @@
+
+const rskapi = require('../..');
+const simpleabi = require('simpleabi');
+const utils = require('./lib/utils');
+const txs = require('./lib/txs');
+
+let config;
+
+try {
+    config = require('./config.json');
+}
+catch (ex) {
+    config = {};
+}
+
+if (!config.instances)
+    config.instances = {};
+
+if (!config.options)
+    config.options = {};
+
+const from = utils.getAccount(config, process.argv[2]);
+const to = utils.getInstanceAddress(config, process.argv[3]);
+const fn = process.argv[4];
+let args = process.argv[5];
+
+if (args)
+    args = args.split(',');
+
+const tx = {
+    to: to,
+    value: 0,
+    gas: config.options.gas || 5000000,
+    gasPrice: config.options.gasPrice || 60000000,
+    data: simpleabi.encodeCall(fn, args)
+};
+
+const host = rskapi.host(config.host);
+
+(async function() {
+    const txh = await txs.send(host, from, tx);
+    console.log('transaction', txh);
+    await txs.receipt(host, txh);
+    console.log('done');
+})();
+
--- samples/commands/lib/txs.js
+++ samples/commands/lib/txs.js
@@ -6,44 +6,6 @@ async function callContract(host, tx) {
     return await host.callTransaction(tx);
 }
 
-async function invokeContract(host, address, fnhash, args, options) {
-    if (options.from.privateKey) {
-        const nonce = await host.getTransactionCount(options.from.address, 'pending');
-        
-        const tx = {
-            gas: options.gas || 1000000,
-            gasPrice: options.gasPrice || 0,
-            value: options.value || 0,
-            to: address,
-            nonce: nonce,
-            data: fnhash + simpleabi.encodeValues(args)
-        };
-        
-        console.dir(tx);
-        
-        const xtx = new Tx(tx);
-        const privateKey = new Buffer(options.from.privateKey.substring(2), 'hex');
-        xtx.sign(privateKey);
-        const serializedTx = xtx.serialize();
-        
-        console.log('0x' + serializedTx.toString('hex'));
-        
-        return await host.sendRawTransaction('0x' + serializedTx.toString('hex'));
-    }
-    else {
-        const tx = {
-            from: options.from,
-            gas: options.gas || 1000000,
-            gasPrice: options.gasPrice || 0,
-            value: options.value || 0,
-            to: address,
-            data: fnhash + simpleabi.encodeValues(args)
-        };
-        
-        return await host.sendTransaction(tx);
-    }
-}
-
 async function transferValue(host, receiver, amount, options) {
     if (options.from.privateKey) {
         const nonce = await host.getTransactionCount(options.from.address, 'pending');
@@ -110,7 +72,6 @@ async function getTransactionReceipt(host, txhash) {
 
 module.exports = {
     call: callContract,
-    invoke: invokeContract,
     transfer: transferValue,
     send: sendTransaction,
     receipt: getTransactionReceipt
