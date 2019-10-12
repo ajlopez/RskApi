e12e3e8dadaf463f3b4afa26773c79a9b82e1cec
ajlopez <webmaster@ajlopez.com>
2019-10-12 16:40:54 -0300

    Get address uses instances if present

--- samples/commands/lib/utils.js
+++ samples/commands/lib/utils.js
@@ -3,14 +3,14 @@ function getAddress(config, user) {
     if (user.substring(0, 2).toLowerCase() === '0x')
         return user;
     
-    if (!config.accounts)
-        return user;
-    
-    if (config.accounts[user])
+    if (config.accounts && config.accounts[user])
         if (config.accounts[user].address)
             return config.accounts[user].address;
         else
             return config.accounts[user];
+        
+    if (config.instances && config.instances[user])
+        return config.instances[user].address;
         
     return user;
 }
