d21ace7d13426893c373814485a8c57ae9a5bfe7
ajlopez <webmaster@ajlopez.com>
2020-02-18 17:55:51 -0300

    Sample trace transaction

---
+++ samples/simple/tracetx.js
@@ -0,0 +1,14 @@
+
+var rskapi = require('../..');
+
+var host = rskapi.host(process.argv[2]);
+var hash = process.argv[3];
+
+host.traceTransaction(hash, function (err, data) {
+	if (err)
+		console.log('error', err);
+	else
+		console.dir(data);
+});
+
+
