a9cdaa15843dcf23092fcb27409b2b990014f860
ajlopez <webmaster@ajlopez.com>
2020-02-29 09:49:12 -0300

    Get logs

---
+++ test/logs.js
@@ -0,0 +1,34 @@
+
+const rskapi = require('..');
+
+exports['get logs'] = function (test) {
+	const provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getLogs = function (filter) {
+        test.ok(filter);
+        test.equal(filter.fromBlock, '0x1');
+        test.equal(filter.toBlock, '0x2a');
+        
+        return 'logs';
+	};
+	
+	const host = rskapi.host(provider);
+	
+	host.getLogs( { fromBlock: 1, toBlock: 42 }, function (err, data) {
+		test.equal(err, null);
+		test.ok(data);
+		test.equal(data, 'logs');
+		test.done();
+	});
+};
+
+function createProvider() {
+	return {
+		call: function (method, args, cb) {
+			cb(null, this[method].apply(this,args));
+		}
+	}
+}
+
