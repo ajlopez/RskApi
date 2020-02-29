a4f8767c1aff25f1bdf89449451980421778f902
ajlopez <webmaster@ajlopez.com>
2020-02-29 14:08:05 -0300

    Async get logs

---
+++ test/asynclogs.js
@@ -0,0 +1,33 @@
+
+const rskapi = require('..');
+
+exports['get logs'] = async function (test) {
+	const provider = createProvider();
+	
+	test.async();
+	
+	provider.eth_getLogs = function (filter) {
+        test.ok(filter);
+        test.equal(filter.fromBlock, '0x1');
+        test.equal(filter.toBlock, '0x2a');
+        
+        return 'logs';
+	};
+	
+	const host = rskapi.host(provider);
+	
+	const result = await host.getLogs( { fromBlock: 1, toBlock: 42 });
+
+    test.ok(result);
+    test.equal(result, 'logs');
+    test.done();
+};
+
+function createProvider() {
+	return {
+		call: function (method, args, cb) {
+			cb(null, this[method].apply(this,args));
+		}
+	}
+}
+
