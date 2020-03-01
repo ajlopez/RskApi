01466e590dba0b94dd9e666a3f6c8c179bb307cc
ajlopez <webmaster@ajlopez.com>
2020-03-01 14:02:07 -0300

    Client transfer, invoke, deploy managing transaction nonce

--- test/client.js
+++ test/client.js
@@ -163,6 +163,15 @@ exports['transfer value using private key'] = async function (test) {
     test.async();
     
     const provider = createProvider();
+
+    let noncecalled = false;
+    
+    provider.eth_getTransactionCount = function (address) {
+        test.equal(address, '0x0000000000000000000000000000000000000001');
+        noncecalled = true;
+        
+        return 12;
+    };
     
 	provider.eth_sendRawTransaction = function (tx) {
         test.ok(tx);
@@ -174,10 +183,44 @@ exports['transfer value using private key'] = async function (test) {
     
     const client = rskapi.client(provider);
     
-    const result = await client.transfer({ privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 3, { gas: 100000, gasPrice: 1000 });
+    const result = await client.transfer({ address: '0x0000000000000000000000000000000000000001', privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 3, { gas: 100000, gasPrice: 1000 });
     
     test.ok(result);
     test.equal(result, '0x2a');
+    test.ok(noncecalled);
+    
+    test.done();
+};
+
+exports['transfer value using private key and nonce in options'] = async function (test) {
+    test.async();
+    
+    const provider = createProvider();
+
+    let noncecalled = false;
+    
+    provider.eth_getTransactionCount = function (address) {
+        test.equal(address, '0x0000000000000000000000000000000000000001');
+        noncecalled = true;
+        
+        return 12;
+    };
+    
+	provider.eth_sendRawTransaction = function (tx) {
+        test.ok(tx);
+        test.equal(typeof tx, 'string');
+        test.equal(tx.substring(0, 2), '0x');
+        
+		return '0x2a';
+	};
+    
+    const client = rskapi.client(provider);
+    
+    const result = await client.transfer({ address: '0x0000000000000000000000000000000000000001', privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 3, { gas: 100000, gasPrice: 1000, nonce: 100 });
+    
+    test.ok(result);
+    test.equal(result, '0x2a');
+    test.ok(!noncecalled);
     
     test.done();
 };
@@ -199,6 +242,7 @@ exports['invoke contract'] = async function (test) {
         test.equal(tx.gasPrice, 42);
         test.equal(tx.gas, 5000000);
         test.equal(tx.value, 0);
+        test.ok(tx.nonce == null);
         test.equal(tx.data, '0x' + simpleabi.encodeCall('symbol()', null));
         
 		return '0x2a';
@@ -227,6 +271,7 @@ exports['invoke contract using options'] = async function (test) {
         test.equal(tx.gasPrice, 100);
         test.equal(tx.gas, 1000000);
         test.equal(tx.value, 10);
+        test.ok(tx.nonce == null);
         test.equal(tx.data, '0x' + simpleabi.encodeCall('balanceOf(address)', [ 1 ]));
         
 		return '0x2a';
@@ -247,6 +292,15 @@ exports['invoke contract using options and private key'] = async function (test)
     
     const provider = createProvider();
     
+    let noncecalled = false;
+    
+    provider.eth_getTransactionCount = function (address) {
+        test.equal(address, '0x0000000000000000000000000000000000000001');
+        noncecalled = true;
+        
+        return 12;
+    };
+    
 	provider.eth_sendRawTransaction = function (tx) {
         test.ok(tx);
         test.equal(typeof tx, 'string');
@@ -257,10 +311,42 @@ exports['invoke contract using options and private key'] = async function (test)
     
     const client = rskapi.client(provider);
     
-    const result = await client.invoke({ privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 'balanceOf(address)', [ 1 ], { gas: 1000000, value: 10, gasPrice: 100 });
+    const result = await client.invoke({ address: '0x0000000000000000000000000000000000000001', privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 'balanceOf(address)', [ 1 ], { gas: 1000000, value: 10, gasPrice: 100 });
     
     test.ok(result);
     test.equal(result, '0x2a');
+    test.ok(noncecalled);
+    
+    test.done();
+};
+
+exports['invoke contract using options with nonce and private key'] = async function (test) {
+    test.async();
+    
+    const provider = createProvider();
+    
+    let noncecalled = false;
+    
+    provider.eth_getTransactionCount = function (hash) {
+        noncecalled = true;
+        return 12;
+    };
+    
+	provider.eth_sendRawTransaction = function (tx) {
+        test.ok(tx);
+        test.equal(typeof tx, 'string');
+        test.equal(tx.substring(0, 2), '0x');
+        
+		return '0x2a';
+	};
+    
+    const client = rskapi.client(provider);
+    
+    const result = await client.invoke({ privateKey: "0xcf449c250f204987e030972c4274fd0947a1721a657923c78fa2f50d41a9dbb7" }, 2, 'balanceOf(address)', [ 1 ], { gas: 1000000, value: 10, gasPrice: 100, nonce: 100 });
+    
+    test.ok(result);
+    test.equal(result, '0x2a');
+    test.ok(!noncecalled);
     
     test.done();
 };
@@ -334,6 +420,7 @@ exports['call contract using options and address'] = async function (test) {
         test.equal(tx.gasPrice, 100);
         test.equal(tx.gas, 1000000);
         test.equal(tx.value, 10);
+        test.ok(tx.nonce == null);
         test.equal(tx.data, '0x' + simpleabi.encodeCall('balanceOf(address)', [ 1 ]));
         
 		return '0x2a';
@@ -342,35 +429,6 @@ exports['call contract using options and address'] = async function (test) {
     const client = rskapi.client(provider);
     
     const result = await client.call({ address: 1 }, 2, 'balanceOf(address)', [ 1 ], { gas: 1000000, value: 10, gasPrice: 100 });
-    
-    test.ok(result);
-    test.equal(result, '0x2a');
-    
-    test.done();
-};
-
-exports['deploy contract using options and address'] = async function (test) {
-    test.async();
-    
-    const code = '0x010203040506';
-    const provider = createProvider();
-    
-	provider.eth_sendTransaction = function (tx) {
-        test.ok(tx);
-        
-        test.equal(tx.from, '0x0000000000000000000000000000000000000001');
-        test.ok(!tx.to);
-        test.equal(tx.gasPrice, 100);
-        test.equal(tx.gas, 1000000);
-        test.equal(tx.value, 10);
-        test.equal(tx.data, code);
-        
-		return '0x2a';
-	};
-    
-    const client = rskapi.client(provider);
-    
-    const result = await client.deploy({ address: 1 }, code, null, { gas: 1000000, value: 10, gasPrice: 100 });
     
     test.ok(result);
     test.equal(result, '0x2a');
