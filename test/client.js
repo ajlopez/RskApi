9ea9bab24c7cc7831543566f3e05e8a6911bcf91
ajlopez <webmaster@ajlopez.com>
2020-03-01 07:55:52 -0300

    Client get account nonce

--- test/client.js
+++ test/client.js
@@ -55,6 +55,28 @@ exports['get balance'] = async function (test) {
     test.done();
 };
 
+exports['get nonce'] = async function (test) {
+    test.async();
+    
+    const provider = createProvider();
+    
+	provider.eth_getTransactionCount = function (address, block) {
+        test.equal(address, '0x0000000000000000000000000000000000000001');
+        test.equal(block, 'latest');
+        
+		return '0x2a';
+	};
+    
+    const client = rskapi.client(provider);
+    
+    const result = await client.nonce(1);
+    
+    test.ok(result);
+    test.equal(result, '0x2a');
+    
+    test.done();
+};
+
 exports['get receipt'] = async function (test) {
     test.async();
     
