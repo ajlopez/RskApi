8739dec266ffcd91615973d1831785043a9c618d
ajlopez <webmaster@ajlopez.com>
2020-03-09 21:53:42 -0300

    Commands get balanances, get gas supplies

---
+++ samples/commands/getbalances.js
@@ -0,0 +1,27 @@
+
+const rskapi = require('../..');
+const utils = require('./lib/utils');
+
+const config = utils.loadConfiguration('./config.json');
+
+const client = rskapi.client(config.host);
+
+(async function() {
+    try {
+        for (let n in config.accounts) {
+            const address = utils.getAddress(config, n);
+            const balance = await client.balance(address);
+            console.log(n, address, parseInt(balance));
+        }
+        
+        for (let n in config.instances) {
+            const address = utils.getAddress(config, n);
+            const balance = await client.balance(address);
+            console.log(n, address, parseInt(balance));
+        }
+    }
+    catch (ex) {
+        console.log(ex);
+    }
+})();
+
---
+++ samples/commands/getgassupplies.js
@@ -0,0 +1,28 @@
+
+const rskapi = require('../..');
+const utils = require('./lib/utils');
+
+const config = utils.loadConfiguration('./config.json');
+
+const client = rskapi.client(config.host);
+
+(async function() {
+    try {
+        const gasPrice = parseInt(await client.host().getGasPrice());
+        for (let n in config.accounts) {
+            const address = utils.getAddress(config, n);
+            const balance = await client.balance(address);
+            console.log(n, address, parseInt(balance) / gasPrice);
+        }
+        
+        for (let n in config.instances) {
+            const address = utils.getAddress(config, n);
+            const balance = await client.balance(address);
+            console.log(n, address, parseInt(balance) / gasPrice);
+        }
+    }
+    catch (ex) {
+        console.log(ex);
+    }
+})();
+
