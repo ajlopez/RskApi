9f90e36df656a5d391690a34bd2c406cce62b2d6
ajlopez <webmaster@ajlopez.com>
2020-03-11 16:26:00 -0300

    Samples gettx, gettxr updated

--- samples/simple/gettx.js
+++ samples/simple/gettx.js
@@ -4,7 +4,7 @@ var rskapi = require('../..');
 var host = rskapi.host(process.argv[2]);
 var hash = process.argv[3];
 
-host.getTransactionByHash(hash, function (err, data) {
+host.getTransaction(hash, function (err, data) {
 	if (err)
 		console.log('error', err);
 	else
--- samples/simple/gettxr.js
+++ samples/simple/gettxr.js
@@ -4,7 +4,7 @@ var rskapi = require('../..');
 var host = rskapi.host(process.argv[2]);
 var hash = process.argv[3];
 
-host.getTransactionReceiptByHash(hash, function (err, data) {
+host.getTransactionReceipt(hash, function (err, data) {
 	if (err)
 		console.log('error', err);
 	else
