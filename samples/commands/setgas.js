dddc15d59a22f84b67d82a66cd2cd63cdf59a9b8
ajlopez <webmaster@ajlopez.com>
2020-03-13 19:25:21 -0300

    setgas command minor refactor

--- samples/commands/setgas.js
+++ samples/commands/setgas.js
@@ -5,9 +5,6 @@ const config = utils.loadConfiguration('./config.json');
 
 const gas = parseInt(process.argv[2]);
 
-if (!config.options)
-    config.options = {};
-
 config.options.gas = gas;
 
 utils.saveConfiguration('./config.json', config);
