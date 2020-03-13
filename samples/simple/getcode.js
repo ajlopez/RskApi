1d4183c7796dd2a9e84389e0b3982ca9d9f99644
ajlopez <webmaster@ajlopez.com>
2020-03-13 18:20:29 -0300

    Get code sample refactor

--- samples/simple/getcode.js
+++ samples/simple/getcode.js
@@ -1,8 +1,8 @@
 
-var rskapi = require('../..');
+const rskapi = require('../..');
 
-var host = rskapi.host(process.argv[2]);
-var hash = process.argv[3];
+const host = rskapi.host(process.argv[2]);
+const hash = process.argv[3];
 
 host.getCode(hash, function (err, data) {
 	if (err)
