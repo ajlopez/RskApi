6d1a8b62536c6a9027420b08bee5181831dbbeae
ajlopez <webmaster@ajlopez.com>
2021-02-12 15:14:35 -0300

    getblock with properties

--- samples/simple/getblock.js
+++ samples/simple/getblock.js
@@ -4,11 +4,21 @@ const rskapi = require('../..');
 const host = rskapi.host(process.argv[2]);
 const number = process.argv[3];
 
+let properties = process.argv[4];
+
+if (properties)
+    properties = properties.split(',');
+
 host.getBlockByNumber(number, function (err, data) {
 	if (err)
 		console.log('error', err);
-	else
-		console.dir(data);
+	else {
+        if (properties)
+            for (let n in properties)
+                console.log(properties[n], data[properties[n]]);
+        else
+            console.dir(data);
+    }
 });
 
 
